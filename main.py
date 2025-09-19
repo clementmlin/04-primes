9c33f26bd0fca2ff9828bb6698b6afb77e94584c
clementmlin <clement8.moulin@gmail.com>
2025-09-19 07:45:12 +0000

    message explicatif

--- main.py
+++ main.py
@@ -4,24 +4,20 @@ from math import sqrt
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    for i in range(2,p):
+        if p%i==0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
 
     print()
-
 
 if __name__ == "__main__":
     main()
